e015a6b31c4a907cc316f0be1aa82ade50e6921f
Smallten <smallten@smallten.me>
2023-11-14 18:45:40 +0800

    feat: 設定 tdesign 國際化為 zh_TW

--- app.vue
+++ app.vue
@@ -1,10 +1,17 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <t-config-provider :global-config="globalConfig">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </t-config-provider>
 </template>
 
 <script setup lang="ts">
+import merge from 'lodash/merge'
+import enConfig from 'tdesign-vue-next/es/locale/zh_TW'
+
+const globalConfig = merge(enConfig)
+
 useHead({
   title: '招生委員會獨門秘笈',
   meta: [{ name: 'description', content: '招生委員會獨門秘笈' }],
