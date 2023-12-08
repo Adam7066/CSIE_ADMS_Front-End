020c5b06969f1f0de02fb1df8f12fe740154cc6a
Smallten <smallten@smallten.me>
2023-12-08 17:16:53 +0800

    feat: 取得學生資料

--- components/analyze/studentData.vue
+++ components/analyze/studentData.vue
@@ -1,202 +1,132 @@
 <template>
-  <div class="h-full w-full">
-    <ClientOnly>
-      <div class="bg-blue-50 px-8 py-12">
-        <div class="flex flex-col items-center justify-center">
-          <div class="m-2 self-start bg-white">
-            <t-select v-model="SiJiSelected" :options="SiJi" placeholder="系級"></t-select>
-          </div>
-          <div class="flex flex-row content-start justify-between">
-            <div
-              class="m-2 min-h-[768px] w-full rounded-md border-2 border-gray-300 bg-green-50 p-4"
-            >
-              <VChart :init-options="initOptions" :option="option" class="h-full w-full" />
-            </div>
-            <div
-              class="m-2 min-h-[768px] w-full rounded-md border-2 border-gray-300 bg-pink-50 p-4"
-            >
-              <t-table
-                :columns="colSmall"
-                :data="stuData"
-                row-key="id"
-                :selected-row-keys="selectedRowKeys"
-                :active-row-type="activeRow ? 'single' : undefined"
-                @select-change="rehandleSelectChange"
-              >
-              </t-table>
-            </div>
-          </div>
+  <ClientOnly>
+    <div class="h-fit min-h-full w-full bg-blue-50 p-8">
+      <div class="flex flex-col items-center justify-center space-y-4">
+        <div class="ml-8 flex space-x-8 self-start">
+          <t-select v-model="yearValue" label="系級：" :options="yearOptions" />
+          <t-button @click="getStuData">取得資料</t-button>
         </div>
+        <div class="w-full rounded-xl border-2 bg-pink-50 p-4">
+          <t-table
+            row-key="id"
+            :columns="columns"
+            :data="stuData"
+            :bordered="true"
+            :max-height="400"
+            :selected-row-keys="selectedRowKeys"
+            :loading="getStuDataPending"
+            @select-change="rehandleSelectChange"
+          />
+        </div>
+
+        <div class="ml-8 self-start">
+          <t-button theme="success" variant="outline">取得所選學生「成績排名」資料</t-button>
+        </div>
+        <div class="h-96 w-full rounded-xl border-2 bg-green-50 p-4"></div>
       </div>
-    </ClientOnly>
-  </div>
+    </div>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
-import VChart from 'vue-echarts'
+import { useAuthStore } from '@/stores/auth'
 
-interface studentData {
-  id: number
-  admission_year: string
-  student_code: string
-  name: string
-  admission_method: string
-  graduated_school: string
+const config = useRuntimeConfig()
+const authStore = useAuthStore()
+
+interface StudentYears {
+  error: string
+  data: number[]
 }
 
-const SiJiSelected = ref()
-const selectedRowKeys = ref([])
-const activeRow = ref(false)
-const stuSelected = ref<studentData[]>([])
-const colSmall = ref([
-  { title: '', colKey: 'row-select', type: 'multiple', width: 50 },
+const getStudentYears = async () => {
+  const { data } = await useFetch<StudentYears>(config.public.apiBase + '/student/years', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + authStore.token,
+    },
+  })
+  if (data.value) {
+    if (data.value.error) {
+      alert(data.value.error)
+      return
+    }
+    yearOptions.value = data.value.data.map((item) => {
+      return { label: (item + 4).toString() + ' 級', value: item }
+    })
+  }
+}
+getStudentYears()
+
+interface YearOptions {
+  label: string
+  value: number
+}
+
+const yearValue = ref('')
+const yearOptions = ref<YearOptions[]>([])
+
+const columns = ref([
+  {
+    colKey: 'row-select',
+    type: 'single',
+    checkProps: { allowUncheck: true },
+  },
   { title: '入學年度', colKey: 'admission_year' },
   { title: '學號', colKey: 'student_code' },
   { title: '姓名', colKey: 'name' },
   { title: '入學方式', colKey: 'admission_method' },
+  { title: '入學組別', colKey: 'admission_group' },
+  { title: '身分類別', colKey: 'identity_category' },
   { title: '畢業學校', colKey: 'graduated_school' },
 ])
-const SiJi = ref([
-  { content: '107級', value: 107 },
-  { content: '108級', value: 108 },
-  { content: '109級', value: 109 },
-  { content: '110級', value: 110 },
-])
-const stuData = ref<studentData[]>([
-  {
-    id: 1,
-    admission_year: '113',
-    student_code: '40947043S',
-    name: '徐政皓',
-    admission_method: '分發入學',
-    graduated_school: '國立新竹高中',
-  },
-  {
-    id: 2,
-    admission_year: '113',
-    student_code: '40947026S',
-    name: '余原齊',
-    admission_method: '特殊申請',
-    graduated_school: '某所私立高中',
-  },
-  {
-    id: 3,
-    admission_year: '113',
-    student_code: '40947030S',
-    name: '陳柏翰',
-    admission_method: '個人申請',
-    graduated_school: '國立建國高中',
-  },
-])
 
-const rehandleSelectChange = (selectRows: []) => {
-  selectedRowKeys.value = selectRows
+interface StuData {
+  id: number
+  name: string
+  student_code: string
+  admission_year: number
+  admission_method: string
+  admission_group: string
+  identity_category: string
+  graduated_school: string
+}
 
-  stuSelected.value = []
-  selectedRowKeys.value.forEach((item: number) => {
-    stuSelected.value.push(stuData.value[item])
+interface StuRes {
+  error: string
+  data: StuData[]
+}
+
+const getStuDataPending = ref(false)
+const stuData = ref<StuData[]>([])
+
+const getStuData = async () => {
+  getStuDataPending.value = true
+  const { data } = await useFetch<StuRes>(config.public.apiBase + '/students', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + authStore.token,
+    },
+    params: {
+      year: yearValue.value,
+    },
   })
+  getStuDataPending.value = false
+  if (data.value) {
+    if (data.value.error) {
+      alert(data.value.error)
+      return
+    }
+    stuData.value = data.value.data
+  }
 }
 
-// chart
-const initOptions = ref({
-  locale: 'EN',
-})
-
-// const option = ref({
-//   xAxis: {
-//     type: 'category',
-//     data: ['110-1', '110-2', '111-1', '111-2', '112-1', '112-2', '113-1', '113-2'],
-//     axisLine: {
-//       onZero: false,
-//     },
-//   },
-//   yAxis: {
-//     name: '排名',
-//     nameLocation: 'start',
-//     type: 'value',
-//     inverse: true,
-//   },
-//   series: [
-//     {
-//       data: [40, 30, 33, 45, 29, 26, 28],
-//       type: 'line',
-//     },
-//   ],
-//   toolbox: {
-//     show: true,
-//     left: 'right',
-//     top: 'top',
-//     feature: {
-//       saveAsImage: {},
-//     },
-//   },
-// })
-
-const option = ref()
-
-// const fetchStuRank = async () => {
-//   const { data } = await useFetch<stuRankResData>(config.public.apiBase + '/stuRank', {
-//     method: 'POST',
-//     body: JSON.stringify(stuSelected.value),
-//   })
-//   stuRank.value = data.data
-// }
-
-const updateChartOption = () => {
-  if (stuSelected.value.length === 0) {
-    // error message
-    return
-  }
-
-  const semester = []
-  for (let i = 0; i < 4; i++) {
-    semester.push(`${SiJiSelected.value + i}-1`)
-    semester.push(`${SiJiSelected.value + i}-2`)
-  }
-
-  const insOption = {
-    title: {
-      text: '學生入學後學期排名追蹤',
-    },
-    xAxis: {
-      name: '學期',
-      nameLocation: 'end',
-      type: 'category',
-      data: semester,
-      axisLine: {
-        onZero: false,
-      },
-    },
-    yAxis: {
-      name: '排名',
-      nameLocation: 'start',
-      type: 'value',
-      inverse: true,
-    },
-    series: [
-      {
-        data: [40, 30, 33, 45, 29, 26, 28],
-        type: 'line',
-      },
-    ],
-    toolbox: {
-      show: true,
-      left: 'right',
-      top: 'top',
-      feature: {
-        saveAsImage: {},
-      },
-    },
-  }
-  option.value = insOption
+const selectedRowKeys = ref<number[]>([])
+const rehandleSelectChange = (
+  value: number[],
+  { selectedRowData }: { selectedRowData: StuData[] },
+) => {
+  selectedRowKeys.value = value
+  console.log(selectedRowData)
 }
-
-watch(stuSelected, () => {
-  updateChartOption()
-})
-
-onUpdated(() => {
-  updateChartOption()
-})
 </script>
