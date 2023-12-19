8d966949680e44ef4f85a1de42d080656cc9051f
Smallten <smallten@smallten.me>
2023-12-19 17:46:46 +0800

    feat: 更新 auth middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,12 +1,34 @@
 import { useAuthStore } from '@/stores/auth'
 
-export default defineNuxtRouteMiddleware((to) => {
+interface IsAdmin {
+  code: number
+  error: string
+  data: boolean
+}
+
+export default defineNuxtRouteMiddleware(async (to) => {
+  const config = useRuntimeConfig()
   const authStore = useAuthStore()
   const isLoggedIn = computed(() => authStore.isLoggedIn)
-  const isAdmin = computed(() => authStore.isAdmin)
 
   if (!isLoggedIn.value && to.path !== '/') return navigateTo('/')
   if (isLoggedIn.value) {
+    const { data } = await useFetch<IsAdmin>(config.public.apiBase + '/is_admin', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + authStore.getToken,
+      },
+    })
+    if (data.value) {
+      if (data.value.code === -1 || data.value.error) {
+        authStore.logout()
+        return navigateTo('/')
+      }
+      authStore.setRole(data.value.data ? 'admin' : 'user')
+    }
+
+    const isAdmin = computed(() => authStore.isAdmin)
+
     if (to.path === '/') return navigateTo('/dashboard')
     if (to.path.includes('admin') && !isAdmin.value) return navigateTo('/dashboard')
   }
