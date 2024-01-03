87be8a0a4282768a244627c509d6079309599908
Smallten <smallten@smallten.me>
2024-01-03 22:49:56 +0800

    feat: 新增資料管理中現有資料表

--- pages/admin/data.vue
+++ pages/admin/data.vue
@@ -30,7 +30,7 @@
         <t-upload
           ref="uploadRef"
           v-model="files"
-          draggable
+          :draggable="true"
           :auto-upload="false"
           :request-method="requestMethod"
           :on-fail="handleUploadFail"
@@ -42,12 +42,36 @@
     <t-divider />
 
     <div class="text-2xl font-semibold">現有資料</div>
+    <div class="flex flex-col space-y-4 p-4">
+      <div class="flex w-96 space-x-8">
+        <t-select v-model="nowDataYearValue" label="系級：" :options="nowDataYearOptions" />
+        <t-button @click="getStuData">取得資料</t-button>
+      </div>
+      <div class="w-full rounded-xl border-2 bg-white p-4">
+        <t-table
+          row-key="id"
+          :columns="columns"
+          :data="stuData"
+          :bordered="true"
+          :max-height="600"
+          :loading="getStuDataPending"
+        >
+          <template #operation="{ row }">
+            <t-button variant="text" theme="primary" @click="editStuData(row.id)">編輯</t-button>
+          </template>
+        </t-table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { type UploadFile, type UploadFailContext, MessagePlugin } from 'tdesign-vue-next'
 import { useAuthStore } from '@/stores/auth'
+
+onMounted(() => {
+  getStudentYears()
+})
 
 const config = useRuntimeConfig()
 const authStore = useAuthStore()
@@ -141,5 +165,90 @@ const requestMethod = async (file: UploadFile) => {
 
 const handleUploadFail = (e: UploadFailContext) => {
   MessagePlugin.error(e.response.error)
+}
+
+interface StudentYears {
+  error: string
+  data: number[]
+}
+
+const getStudentYears = async () => {
+  const { data } = await useFetch<StudentYears>(config.public.apiBase + '/student/years', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + authStore.token,
+    },
+  })
+  if (data.value) {
+    if (data.value.error) {
+      alert(data.value.error)
+      return
+    }
+    nowDataYearOptions.value = data.value.data.map((item) => {
+      return { label: (item + 4).toString() + ' 級', value: item }
+    })
+  }
+}
+
+const nowDataYearValue = ref('')
+const nowDataYearOptions = ref<
+  {
+    label: string
+    value: number
+  }[]
+>([])
+
+const columns = ref([
+  { title: '入學年度', colKey: 'admission_year' },
+  { title: '學號', colKey: 'student_code' },
+  { title: '姓名', colKey: 'name' },
+  { title: '入學方式', colKey: 'admission_method' },
+  { title: '入學組別', colKey: 'admission_group' },
+  { title: '身分類別', colKey: 'identity_category' },
+  { title: '畢業學校', colKey: 'graduated_school' },
+  { title: '操作', colKey: 'operation' },
+])
+
+interface StuData {
+  id: number
+  name: string
+  student_code: string
+  admission_year: number
+  admission_method: string
+  admission_group: string
+  identity_category: string
+  graduated_school: string
+}
+
+interface StuRes {
+  error: string
+  data: StuData[]
+}
+
+const getStuDataPending = ref(false)
+const stuData = ref<StuData[]>([])
+const getStuData = async () => {
+  getStuDataPending.value = true
+  const { data: getStuData } = await useFetch<StuRes>(config.public.apiBase + '/students', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + authStore.token,
+    },
+    params: {
+      year: nowDataYearValue.value,
+    },
+  })
+  if (getStuData.value) {
+    if (getStuData.value.error) {
+      alert(getStuData.value.error)
+      return
+    }
+    stuData.value = getStuData.value.data
+  }
+  getStuDataPending.value = false
+}
+
+const editStuData = (id: number) => {
+  console.log(id)
 }
 </script>
