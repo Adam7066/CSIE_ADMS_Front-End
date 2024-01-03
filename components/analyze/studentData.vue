e50809d589bea0170e3c108fb6d1b44204bdcf98
HsuJinho <hsujinho@gmail.com>
2024-01-03 22:23:21 +0800

    fix: 新增散點圖分頁

--- components/analyze/studentData.vue
+++ components/analyze/studentData.vue
@@ -4,14 +4,13 @@
       <div class="flex flex-col items-center justify-center space-y-4">
         <div class="ml-8 flex space-x-8 self-start">
           <t-select v-model="yearValue" label="系級：" :options="yearOptions" />
-          <t-button @click="getStuData">取得資料</t-button>
+          <t-button @click="onSiJiButtonClick">取得資料</t-button>
         </div>
-        <div class="w-full rounded-xl border-2 bg-white p-4">
+        <div class="min-h-[438px] w-full rounded-xl border-2 bg-gray-100 p-4">
           <t-table
             row-key="id"
             :columns="columns"
             :data="stuData"
-            :bordered="true"
             :max-height="400"
             :selected-row-keys="selectedRowKeys"
             :loading="getStuDataPending"
@@ -19,12 +18,51 @@
           />
         </div>
 
-        <div
-          v-if="seriesDataSemester.length + seriesDataYears.length > 0"
-          class="h-[800px] w-full rounded-xl border-2 bg-white p-4"
-        >
-          <VChart :option="optionSemester" :autoresize="true" class="h-1/2" />
-          <VChart :option="optionYears" :autoresize="true" class="h-1/2" />
+        <div class="min-h-[768px] w-full rounded-md border-2 border-gray-300 bg-white">
+          <t-tabs v-model="currentTab" theme="card">
+            <t-tab-panel
+              :value="1"
+              label="當學期成績排名折線圖"
+              :destroy-on-hide="false"
+              :lazy="true"
+            >
+              <div
+                v-if="seriesDataSemester.length + seriesDataYears.length > 0"
+                class="h-[800px] w-full bg-white p-4"
+              >
+                <VChart :option="optionSemester" :autoresize="true" class="h-1/2" />
+              </div>
+              <div v-else>
+                <div class="text-xl">請先勾選學生資料</div>
+              </div>
+            </t-tab-panel>
+            <t-tab-panel
+              :value="2"
+              label="歷年成績排名折線圖"
+              :destroy-on-hide="false"
+              :lazy="true"
+            >
+              <div
+                v-if="seriesDataSemester.length + seriesDataYears.length > 0"
+                class="h-[800px] w-full bg-white p-4"
+              >
+                <VChart :option="optionYears" :autoresize="true" class="h-1/2" />
+              </div>
+              <div v-else>
+                <div class="text-xl">請先勾選學生資料</div>
+              </div>
+            </t-tab-panel>
+            <t-tab-panel
+              :value="3"
+              label="當學期分組排名散點圖"
+              :destroy-on-hide="false"
+              :lazy="true"
+            >
+              <div v-if="yearValue" class="h-[800px] w-full bg-white p-4">
+                <VChart :option="optionGroupScatter" :autoresize="true" class="h-full" />
+              </div>
+            </t-tab-panel>
+          </t-tabs>
         </div>
       </div>
     </div>
@@ -90,6 +128,8 @@ const columns = ref([
   { title: '身分類別', colKey: 'identity_category' },
   { title: '畢業學校', colKey: 'graduated_school' },
 ])
+
+const currentTab = ref('1')
 
 interface StuData {
   id: number
@@ -165,13 +205,61 @@ const getStuData = async () => {
   getStuDataPending.value = false
 }
 
+const methods = computed(() => {
+  return Array.from(new Set(stuData.value.map((item) => item.admission_method)))
+})
+
+const updateScatterOption = () => {
+  seriesScatterDataSemester.value = []
+  const year1 = yearValue.value + '-1'
+  const year2 = yearValue.value + '-2'
+  const year1Data = [] as number[][]
+  const year2Data = [] as number[][]
+  for (let i = 0; i < stuData.value.length; i++) {
+    if (rankData.value[i].semester[year1]) {
+      year1Data.push([
+        methods.value.findIndex((item) => item === stuData.value[i].admission_method),
+        rankData.value[i].semester[year1],
+        i,
+      ])
+    }
+    if (rankData.value[i].semester[year2]) {
+      year2Data.push([
+        methods.value.findIndex((item) => item === stuData.value[i].admission_method),
+        rankData.value[i].semester[year2],
+        i,
+      ])
+    }
+  }
+  seriesScatterDataSemester.value.push({
+    name: year1,
+    type: 'scatter',
+    data: year1Data,
+  })
+  seriesScatterDataSemester.value.push({
+    name: year2,
+    type: 'scatter',
+    data: year2Data,
+  })
+
+  legendDataScatter.value = [year1, year2]
+  optionGroupScatter.value.legend.data = legendDataScatter.value
+  optionGroupScatter.value.series = seriesScatterDataSemester.value
+  optionGroupScatter.value.xAxis.data = methods.value
+}
+
+const onSiJiButtonClick = async () => {
+  await getStuData()
+  updateScatterOption()
+}
+
 const selectedRowKeys = ref<number[]>([])
 const rehandleSelectChange = (
   value: number[],
   { selectedRowData }: { selectedRowData: StuData[] },
 ) => {
   selectedRowKeys.value = value
-  legendData.value = selectedRowData.map((item) => item.student_code)
+  legendData.value = selectedRowData.map((item) => item.name)
   xAxisDataSemester.value = Array.from(
     new Set(rankData.value.flatMap((item) => Object.keys(item.semester))),
   ).sort()
@@ -276,5 +364,50 @@ const optionYears = ref({
     min: 1,
   },
   series: seriesDataYears.value,
+})
+
+const legendDataScatter = ref<string[]>([])
+const seriesScatterDataSemester = ref([]) as Ref<{ name: string; type: string; data: number[][] }[]>
+const optionGroupScatter = ref({
+  title: {
+    text: '當學期入學方式排名',
+  },
+  tooltip: {
+    trigger: 'axis',
+    order: 'valueAsc',
+    formatter: function (params: Array<{ seriesName: string; name: string; value: number[] }>) {
+      return (
+        params[0].name +
+        '<br/>' +
+        params
+          .map((item: any) => stuData.value[item.value[2]].name + ': ' + item.value[1])
+          .join('<br/>')
+      )
+    },
+  },
+  legend: {
+    data: legendDataScatter.value,
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {},
+    },
+  },
+  grid: {
+    left: 2,
+    bottom: 10,
+    right: 10,
+    containLabel: true,
+  },
+  xAxis: {
+    type: 'category',
+    data: methods.value,
+  },
+  yAxis: {
+    type: 'value',
+    inverse: true,
+    min: 1,
+  },
+  series: seriesScatterDataSemester.value,
 })
 </script>
