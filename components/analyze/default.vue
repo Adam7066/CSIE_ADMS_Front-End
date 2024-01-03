dbeffac0e281a71116e93c98b8ad864045247fd8
Smallten <smallten@smallten.me>
2024-01-03 16:18:36 +0800

    feat: 增加數據分析頁面背景顏色

--- components/analyze/default.vue
+++ components/analyze/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex h-full w-full justify-center px-8 py-12">
-    <div class="flex h-80 w-2/3 items-center justify-center rounded-xl bg-white">
+  <div class="flex h-full w-full justify-center bg-blue-50 px-8 py-12">
+    <div class="flex h-80 w-2/3 items-center justify-center rounded-xl border bg-white">
       <div class="text-4xl">
         歡迎使用
         <span class="font-semibold">數據分析</span>
